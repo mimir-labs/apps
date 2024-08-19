e1f3f9f513d9c7e0f154195d0c7f55d9ad259260
jarvandev <jarvan.z@outlook.com>
2024-08-20 01:18:57 +0800

    add avail types

--- packages/apps-config/src/api/index.ts
+++ packages/apps-config/src/api/index.ts
@@ -1,10 +1,19 @@
 // Copyright 2017-2024 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import avail from './avail.js';
 import { applyDerives } from './derives.js';
 import { typesBundle as typesOnlyBundle } from './typesBundle.js';
 
 export * from './constants.js';
 export * from './params/index.js';
 
-export const typesBundle = /*#__PURE__*/ applyDerives(typesOnlyBundle);
+const _typesBundle = /*#__PURE__*/ applyDerives(typesOnlyBundle);
+
+export const typesBundle = {
+  ..._typesBundle,
+  spec: {
+    ..._typesBundle.spec,
+    avail
+  }
+};
